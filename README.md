87e4fec0e055c31c5040f83a3d0879d380c61c7c
norcm <cuixinpeng1157@gmail.com>
2023-08-10 16:50:24 +0800

    update
    (cherry picked from commit bf71ed96c2f139e8d7b23a4282e423e11c7c4440)

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 [![Node.js CI](https://github.com/norcm/iCal/actions/workflows/node.js.yml/badge.svg?branch=master&event=push)](https://github.com/norcm/iCal/actions/workflows/node.js.yml)
-# 国家法定假日(iCal 格式)
-
-已有的法定假日大部分不维护，索性自己写个。
-
-有需要可以 clone 下来自己修改。
-
-不局限节假日，任意活动都可以自行编辑。
 
 ## 订阅地址
 
@@ -18,45 +11,6 @@ yarn install
 yarn run build
 ```
 
-## 项目结构
-
-```js
-.
-├── README.md
-├── data
-│   ├── 2018.json // 2018年节日
-│   └── 2019.json // 2019年节日
-├── dist
-│   └── holidays.ics
-├── index.js
-├── package.json
-└── yarn.lock
-```
-
-## 如何修改
-
-元数据保存在 */data/*.json* 文件中，格式如下：
-
-```js
-[{
-    "DTSTART;VALUE=DATE": "20180924",
-    "DTEND;VALUE=DATE": "20180924",
-    "SUMMARY": "中秋节"
-},{
-    "DTSTART;VALUE=DATE": "20180929",
-    "DTEND;VALUE=DATE": "201801001",
-    "SUMMARY": "国庆补班"
-},{
-    "DTSTART;VALUE=DATE": "20181001",
-    "DTEND;VALUE=DATE": "20181008",
-    "SUMMARY": "国庆放假"
-}]
-```
-
-> 结束日期不包含当天。如果想要连续创建活动，结束时间一定要大一天。
-
 ## 参考
 
 [https://zh.wikipedia.org/zh/ICalendar](https://zh.wikipedia.org/zh/ICalendar)
-
-[https://gist.github.com/yulanggong/be953ffee1d42df53a1a](https://gist.github.com/yulanggong/be953ffee1d42df53a1a)
--- index.js
+++ index.js
@@ -1,49 +1,84 @@
 const fs = require('fs-extra');
 const path = require('path');
 const glob = require('glob');
+const {Solar, SolarMonth, HolidayUtil} = require('lunar-typescript');
 
-// 最终的文件名
 const filename = path.join(__dirname, 'dist', 'holidays.ics');
 
-// 加载 event
-async function loadEvent() {
-    const node = [];
-    const scanPattern = 'data/**.json';
-    const files = glob.sync(scanPattern);
-    for (let i = 0, l = files.length; i < l; i++) {
-        const filePath = path.join(__dirname, files[i]);
-        const json = await fs.readJson(filePath);
-        
-        json.forEach(item => {
-            node.push('BEGIN:VEVENT');
-            Object.entries(item).forEach(child => {
-                const [ key, value ] = child;
-                node.push(`${key}:${value}`);
-            });
-            node.push('SEQUENCE:0');
-            node.push('END:VEVENT');
-        });
-        
+function loadEvents() {
+    let events = [];
+    const start = new Date("2020-01-01");
+    const end = new Date();
+    end.setFullYear(end.getFullYear() + 3, 1, 1);
+    let currentDay = start;
+    while (currentDay.getTime() <= end.getTime()) {
+        let festival = getFestival(Solar.fromDate(currentDay));
+        if (festival && festival.length > 0) {
+            let event = buildEvent(festival, `${currentDay.getFullYear()}-${currentDay.getMonth() + 1}-${currentDay.getDate()}`);
+            events.push(...event)
+        }
+        currentDay = nextDate(currentDay);
     }
-    return node;
+    return events;
 }
 
+
+function getFestival(d) {
+    const lunar = d.getLunar()
+    let festival = ''
+    let otherFestivals = d.getOtherFestivals()
+    if (otherFestivals.length > 0) {
+        festival = otherFestivals[0]
+    }
+    otherFestivals = lunar.getOtherFestivals()
+    if (otherFestivals.length > 0) {
+        festival = otherFestivals[0]
+    }
+    let festivals = d.getFestivals()
+    if (festivals.length > 0) {
+        festival = festivals[0]
+    }
+    festivals = lunar.getFestivals()
+    if (festivals.length > 0) {
+        festival = festivals[0]
+    }
+    return festival
+}
+
+
+function buildEvent(summary, day) {
+    let event = [];
+    event.push('BEGIN:VEVENT');
+    event.push(`DTSTART;VALUE=DATE:${day}`)
+    event.push(`DTEND;VALUE=DATE:${day}`)
+    event.push(`SUMMARY:${summary}`)
+    event.push('SEQUENCE:0');
+    event.push('END:VEVENT');
+    return event;
+}
+
+function nextDate(day) {
+    let nextDay = new Date(day);
+    nextDay.setDate(day.getDate() + 1);
+    return nextDay;
+}
+
+
 // 输出文件
-async function output(file) {
+function build(file) {
     let nodes = ['BEGIN:VCALENDAR'];
     nodes.push('VERSION:2.0');
-    nodes.push('X-WR-CALNAME:法定节假日');
+    nodes.push('X-WR-CALNAME:节假日');
     nodes.push('X-WR-CALDESC:');
     nodes.push('X-APPLE-CALENDAR-COLOR:#65db39FF');
-
     // 追加 event
-    const events = await loadEvent();
+    const events = loadEvents();
     nodes = nodes.concat(events);
 
     nodes.push('END:VCALENDAR');
 
-    const content = nodes.join('\r\n');
-    await fs.outputFile(file, content);
+    const content = nodes.join('\n');
+    fs.outputFile(file, content);
 }
 
-output(filename);
+build(filename);
